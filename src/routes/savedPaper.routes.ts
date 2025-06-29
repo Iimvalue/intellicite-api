439e0de17c6dba49db47edf61f1c7d001aebd263
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-06-29 19:26:52 +0300

    add postman json and sort folders

--- src/routes/savedPaper.routes.ts
+++ src/routes/savedPaper.routes.ts
@@ -11,7 +11,7 @@ const router = Router();
 
 router.post('/', authenticate, addSavedPaper);
 router.get('/', authenticate, getSavedPapers);
-router.put('/:paperId/notes', authenticate, updateSavedPaperNotes);
+router.put('/:paperId', authenticate, updateSavedPaperNotes);  
 router.delete('/:paperId', authenticate, deleteSavedPaper);
 
 export default router;
