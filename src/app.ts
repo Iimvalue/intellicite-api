9b6970a7b7ab0229fb90fa5fba1e4a8c03595e22
Entisar <Code.g.light@gmail.com>
2025-06-29 10:14:02 +0300

    move it to a shared repo

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import { connectDB } from './config/database';
+import userRoutes from './routes/users.routes';
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+const app: Application = express();
+
+connectDB()
+  .then(() => console.info('Database connected successfully'))
+
+// Middlewares
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Routes
+app.use('/api/users', userRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ 
+    message: 'Welcome to IntelliCite',
+    status: 'Server is running successfully',
+    timestamp: new Date().toISOString()
+  });
+});
+
+app.listen(port, () => {
+    console.info(`Server is running on port ${port}`);
+  });
+export default app;
+
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+} from '../controllers/users.controller';
+import { authenticate } from '../middlewares/auth.middleware';
+
+const router = Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+router.get('/profile', authenticate, getUserProfile);
+router.put('/profile', authenticate, updateUserProfile);
+
+export default router;
