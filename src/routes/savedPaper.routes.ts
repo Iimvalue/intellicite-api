b73120fdc6a5f317b913c9776928ea6109489092
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-06-29 17:47:48 +0300

    upload

---
+++ src/routes/savedPaper.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import {
+  addSavedPaper,
+  deleteSavedPaper,
+  getSavedPapers,
+  updateSavedPaperNotes,
+} from '../controllers/savedPaper.controller';
+import { authenticate } from '../middlewares/auth.middleware';
+
+const router = Router();
+
+router.post('/', authenticate, addSavedPaper);
+router.get('/', authenticate, getSavedPapers);
+router.put('/:paperId/notes', authenticate, updateSavedPaperNotes);
+router.delete('/:paperId', authenticate, deleteSavedPaper);
+
+export default router;
