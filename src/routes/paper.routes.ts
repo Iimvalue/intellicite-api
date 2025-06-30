a16606f181dc40270ad19da3fa9cd74a628b8055
entisarosaimi <code.g.light@gmail.com>
2025-06-30 19:03:29 +0300

    rm the cache

--- src/routes/paper.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { searchPapersHandler } from '../controllers/papers.controller';
-import { authenticate } from '../middlewares/auth.middleware';
-
-const router = Router();
-
-router.get('/papers/search',authenticate, searchPapersHandler);
-
-export default router;
