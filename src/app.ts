3595338486ac2dab14213fb08efb575cb4a53eda
entisarosaimi <code.g.light@gmail.com>
2025-06-30 19:30:29 +0300

    this should fix the paper retrieval issues -

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/database";
 import userRoutes from "./routes/users.routes";
 import savedPaperRoutes from "./routes/savedPaper.routes";
-import paperRoutes from "./routes/papers.routes";
+import paperRoutes from "./routes/paper.routes";
 import userHistoryRoutes from "./routes/userHistory.routes";
 import citeCheckRoutes from "./routes/citeCheck.routes";
 dotenv.config();
@@ -24,7 +24,7 @@ app.use(express.static("public"));
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/saved-papers", savedPaperRoutes);
-app.use("/api/papers", paperRoutes);
+app.use("/api", paperRoutes);
 app.use("/api/user-history", userHistoryRoutes);
 app.use("/api/cite-check", citeCheckRoutes);
 
