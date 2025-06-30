adb37c7c2c971b88c3b9b3d521228f5a764419d3
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-06-30 11:16:06 +0300

    add query by id  for userHistory

--- src/routes/userHistory.routes.ts
+++ src/routes/userHistory.routes.ts
@@ -2,14 +2,15 @@
 import express from "express";
 import { authenticate } from "../middlewares/auth.middleware";
 import {
-  getUserSearchHistory,
-  addUserSearchHistory,
-  deleteSingleSearchHistory,
-} from "../controllers/userHistory.controller";
-
+    getUserSearchHistory,
+    addUserSearchHistory,
+    deleteSingleSearchHistory,
+    getSingleSearchHistory
+  } from '../controllers/userHistory.controller';
 const router = express.Router();
 
 router.get("/", authenticate, getUserSearchHistory);
+router.get('/:historyId', authenticate, getSingleSearchHistory);
 router.post("/", authenticate, addUserSearchHistory);
 router.delete("/:historyId", authenticate, deleteSingleSearchHistory);
 
