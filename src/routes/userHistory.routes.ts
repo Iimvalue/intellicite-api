6df31a5bf4726ead8b66c9f18c34f4f642c6f9d5
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-06-30 11:04:20 +0300

    add delete method for userHistory

--- src/routes/userHistory.routes.ts
+++ src/routes/userHistory.routes.ts
@@ -1,11 +1,16 @@
 // routes/userHistory.routes.ts
-import express from 'express';
-import { authenticate } from '../middlewares/auth.middleware';
-import { getUserSearchHistory, addUserSearchHistory } from '../controllers/userHistory.controller';
+import express from "express";
+import { authenticate } from "../middlewares/auth.middleware";
+import {
+  getUserSearchHistory,
+  addUserSearchHistory,
+  deleteSingleSearchHistory,
+} from "../controllers/userHistory.controller";
 
 const router = express.Router();
 
-router.get('/', authenticate, getUserSearchHistory);
-router.post('/', authenticate, addUserSearchHistory);
+router.get("/", authenticate, getUserSearchHistory);
+router.post("/", authenticate, addUserSearchHistory);
+router.delete("/:historyId", authenticate, deleteSingleSearchHistory);
 
 export default router;
